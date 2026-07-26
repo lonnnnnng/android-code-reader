bc12480ee5e8f32684b5f38254116c62e42384a6
guohailong <1105744725@qq.com>
2026-07-27 03:03:57 +0800

    发布 0.1.6 维护版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lonnnnnng.codereader"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
